3b9a1abfe349a96acaa9871f50ceeec5dc4fd9d7
Carlos Hernández Gómez <carlos.hernandez@socialbro.com>
2016-11-06 19:48:44 +0100

    code cleanup

--- gw2/src/camera.js
+++ gw2/src/camera.js
@@ -1,7 +1,7 @@
-var offsets   = require ("./offsets");
-var Spectate  = require ("./spectate");
-var robot     = require ("robot-js");
-var Tweenable = require ("shifty");
+/* eslint-disable no-console */
+var offsets   = require('./offsets');
+var robot     = require('robot-js');
+var Tweenable = require('shifty');
 
 Tweenable.prototype.formula.linear = function (pos) {
   return pos;
@@ -17,7 +17,6 @@ module.exports = (process, module, memory, window, player, sendMessage) => {
   const cameraFwdReadPosition   = new Buffer(0xC);
   const cameraWritePosition     = new Buffer(0xC);
   const cameraFwdWritePosition  = new Buffer(0xC);
-  const playerPosition          = new Buffer(0xC);
 
   const cameraRoll  = new Buffer(0x4);
   const cameraYaw   = new Buffer(0x4);
@@ -26,99 +25,98 @@ module.exports = (process, module, memory, window, player, sendMessage) => {
 
   let cameraOffsetBase  = memory.readMultiLevelPtr(offsets.camera.ptr);
 
-  const spectate = Spectate(process, module, memory, that);
-  let pitchDown = 0, pitchUp = 0, yawRight = 0, yawLeft = 0, rollRight = 0, rollLeft = 0;
-
   let cameraCurrPosition = {
     x: null,
     y: null,
-    z: null,
+    z: null
   };
 
   let cameraFwdCurrPosition = {
     x: null,
     y: null,
-    z: null,
+    z: null
   };
 
-  let cameraRotation = {
-    x: 0,
-    y: 0,
-    z: 0,
+  let environment;
+
+  /**
+   * Refactor this to a non-blocking way
+   * @param  {Number} sleepDuration how much time
+   * @return {undefined}
+   */
+  var sleepFor = function(sleepDuration) {
+    var now = new Date().getTime();
+    while (new Date().getTime() < now + sleepDuration) { /* do nothing */ }
   };
-
-	let environment;
-
-  function sleepFor( sleepDuration ){
-      var now = new Date().getTime();
-      while(new Date().getTime() < now + sleepDuration){ /* do nothing */ }
-  }
 
   that.setEnvReference = (env) => {
-		environment = env;
-	}
-  that.moveTo = (x , y , z, fx, fy, fz, wait) => {
-    while (true) {
-        var cameraPos = that.getPosition();
-        var dx = x - cameraPos.x;
-        var dy = y - cameraPos.y;
-        var dz = z - cameraPos.z;
+    environment = env;
+  };
 
-        var cameraFwdPos = that.getFwdPosition();
-        var fwddx = fx - cameraFwdPos.x;
-        var fwddy = fy - cameraFwdPos.y;
-        var fwddz = fz - cameraFwdPos.z;
-        var v = 0.009;
+  that.moveTo = (x, y, z, fx, fy, fz, wait) => {
+    var truthy = true;
+    while (truthy) {
+      var cameraPos = that.getPosition();
+      var dx = x - cameraPos.x;
+      var dy = y - cameraPos.y;
+      var dz = z - cameraPos.z;
 
-        var dist = Math.sqrt(dx * dx + dy * dy + dz * dz);
-        if (dist <= v) {
-            console.log("Reached the point!");
-            if (wait) {
-              sleepFor(2000);
-            }
-            break;
-        } else {
-          cameraWritePosition.writeFloatLE(cameraPos.x + (v * dx / dist), 0x0);
-          cameraWritePosition.writeFloatLE(cameraPos.y + (v * dy / dist), 0x4);
-          cameraWritePosition.writeFloatLE(cameraPos.z + (v * dz / dist), 0x8);
-          cameraFwdWritePosition.writeFloatLE(cameraFwdPos.x + (v * fwddx / dist), 0x0);
-          cameraFwdWritePosition.writeFloatLE(cameraFwdPos.y + (v * fwddy / dist), 0x4);
-          cameraFwdWritePosition.writeFloatLE(cameraFwdPos.z + (v * fwddz / dist), 0x8);
-          memory.writeData(cameraOffsetBase + offsets.camera.pos.x, cameraWritePosition, 0xC);
-          memory.writeData(cameraOffsetBase + offsets.camera.playerPos.x, cameraFwdWritePosition, 0xC);
+      var cameraFwdPos = that.getFwdPosition();
+      var fwddx = fx - cameraFwdPos.x;
+      var fwddy = fy - cameraFwdPos.y;
+      var fwddz = fz - cameraFwdPos.z;
+      var v = 0.009;
+
+      var dist = Math.sqrt(dx * dx + dy * dy + dz * dz);
+      if (dist <= v) {
+        console.log('Reached the point!');
+        if (wait) {
+          sleepFor(2000);
         }
+        break;
+      } else {
+        var camBase = cameraOffsetBase + offsets.camera.pos.x;
+        var lookT = cameraOffsetBase + offsets.camera.playerPos.x;
+        cameraWritePosition.writeFloatLE(cameraPos.x + (v * dx / dist), 0x0);
+        cameraWritePosition.writeFloatLE(cameraPos.y + (v * dy / dist), 0x4);
+        cameraWritePosition.writeFloatLE(cameraPos.z + (v * dz / dist), 0x8);
+        cameraFwdWritePosition.writeFloatLE(cameraFwdPos.x + (v * fwddx / dist), 0x0);
+        cameraFwdWritePosition.writeFloatLE(cameraFwdPos.y + (v * fwddy / dist), 0x4);
+        cameraFwdWritePosition.writeFloatLE(cameraFwdPos.z + (v * fwddz / dist), 0x8);
+        memory.writeData(camBase, cameraWritePosition, 0xC);
+        memory.writeData(lookT, cameraFwdWritePosition, 0xC);
+      }
     }
-  }
+  };
 
-  that.setPos = (x , y , z) => {
+  that.setPos = (x, y, z) => {
     cameraWritePosition.writeFloatLE(x, 0x0);
     cameraWritePosition.writeFloatLE(y, 0x4);
     cameraWritePosition.writeFloatLE(z, 0x8);
     memory.writeData(cameraOffsetBase + offsets.camera.pos.x, cameraWritePosition, 0xC);
-  }
+  };
 
-  that.lookAt = (x , y , z) => {
+  that.lookAt = (x, y, z) => {
     cameraFwdWritePosition.writeFloatLE(x, 0x0);
     cameraFwdWritePosition.writeFloatLE(y, 0x4);
     cameraFwdWritePosition.writeFloatLE(z, 0x8);
     memory.writeData(cameraOffsetBase + offsets.camera.playerPos.x, cameraFwdWritePosition, 0xC);
-  }
+  };
 
   that.moveToWithTween = (from, to, type, cb) => {
-    console.log(type);
     var tweenable = new Tweenable();
     tweenable.tween({
       from: from,
       to: to,
       duration: 2000,
       easing: type,
-      finish: function(state){ cb(); },
+      finish: function() { cb(); },
       step: function (state) {
-        that.setPos(state.x , state.y , state.z);
-        that.lookAt(state.lookx , state.looky , state.lookz);
+        that.setPos(state.x, state.y, state.z);
+        that.lookAt(state.lookx, state.looky, state.lookz);
       }
     });
-  }
+  };
 
   that.moveUp = () => {
     var cameraPos = that.getPosition();
@@ -126,11 +124,11 @@ module.exports = (process, module, memory, window, player, sendMessage) => {
     cameraWritePosition.writeFloatLE(cameraPos.y, 0x4);
     cameraWritePosition.writeFloatLE(cameraPos.z + -0.03, 0x8);
     memory.writeData(cameraOffsetBase + offsets.camera.pos.x, cameraWritePosition, 0xC);
-  }
+  };
 
   that.updateOffsetBase = () => {
     cameraOffsetBase = memory.readMultiLevelPtr(offsets.camera.ptr);
-  }
+  };
 
   that.getPosition = () => {
     memory.readData(cameraOffsetBase + offsets.camera.pos.x, cameraReadPosition, 0xC);
@@ -147,65 +145,71 @@ module.exports = (process, module, memory, window, player, sendMessage) => {
     cameraFwdCurrPosition.z = cameraFwdReadPosition.readFloatLE(8);
     return cameraFwdCurrPosition;
   };
-
+  /* eslint-disable */
   that.updateRotationVector = () => {
     const update = () => {
       let curFwd = that.getFwdPosition();
-      cameraFwdWritePosition.writeFloatLE( curFwd.x - ( - pitchDown + pitchUp ), 0x0);
-      cameraFwdWritePosition.writeFloatLE( curFwd.y - ( - yawRight  + yawLeft ), 0x4);
-      cameraFwdWritePosition.writeFloatLE( curFwd.z - ( - rollRight + rollLeft), 0x8);
+      cameraFwdWritePosition.writeFloatLE(curFwd.x - (- pitchDown + pitchUp ), 0x0);
+      cameraFwdWritePosition.writeFloatLE(curFwd.y - (- yawRight  + yawLeft ), 0x4);
+      cameraFwdWritePosition.writeFloatLE(curFwd.z - (- rollRight + rollLeft), 0x8);
       memory.writeData(cameraOffsetBase + offsets.camera.playerPos.x, cameraFwdWritePosition, 0xC);
     };
 
     const getContainerDimensions = () => {
       const boundingClientRect = window.getBounds();
       return {
-				size	: [ boundingClientRect.w, boundingClientRect.h ],
-				offset	: [ boundingClientRect.x,  boundingClientRect.y ]
-			};
-    }
-
+        size: [boundingClientRect.w, boundingClientRect.h],
+        offset: [boundingClientRect.x,  boundingClientRect.y]
+      };
+    };
+    /* eslint-enable */
     // mouseMove
     const start = () => {
       setInterval(() => {
         const container    = getContainerDimensions();
         const mouse        = Mouse.getPos();
-  			const halfWidth    = container.size[ 0 ] / 2;
-  			const halfHeight   = container.size[ 1 ] / 2;
+        const halfWidth    = container.size[0] / 2;
+        const halfHeight   = container.size[1] / 2;
+        /* eslint-disable no-unused-vars */
+        let yawLeft   = - ((mouse.x - container.offset[0]) - halfWidth);
+        /* eslint-disable no-unused-vars */
+        let pitchDown =   ((mouse.y - container.offset[1]) - halfHeight);
 
-  			yawLeft   = - ( ( mouse.x - container.offset[ 0 ] ) - halfWidth  );
-  			pitchDown =   ( ( mouse.y - container.offset[ 1 ] ) - halfHeight );
-
-  			update();
+        update();
       }, 0);
     };
     start();
-  }
+  };
 
   let cameraControls;
 
   that.disableCameraControls = () => {
     clearInterval(cameraControls);
-  }
+  };
 
   that.enableCameraControls = () => {
+    /* eslint-disable no-unused-vars */
     const getContainerDimensions = () => {
       const boundingClientRect = window.getBounds();
       return {
-        size	: [ boundingClientRect.w, boundingClientRect.h ],
-        offset	: [ boundingClientRect.x,  boundingClientRect.y ]
+        size: [boundingClientRect.w, boundingClientRect.h],
+        offset: [boundingClientRect.x,  boundingClientRect.y]
       };
     };
 
+    /**
+     * Refactor this to support multiple keys
+     * @type {Number}
+     */
     var notyet = 0;
     function clearTimer() {
-        notyet = 0;
+      notyet = 0;
     }
 
     const checkForKeyStroke = (key) => {
-      if( Keyboard.getState(key) ){
-        if (notyet == 1) {
-            return;
+      if (Keyboard.getState(key)) {
+        if (notyet === 1) {
+          return;
         }
         notyet = 1;
         setTimeout(clearTimer, 300);
@@ -216,42 +220,26 @@ module.exports = (process, module, memory, window, player, sendMessage) => {
     let rotSpeed = 0.02;
     let speed = 8;
 
-    let curFwd    = that.getFwdPosition();
-    let cameraPos = that.getPosition();
-
-    var lastUpdate = Date.now();
-    lastx = 0;
-    lasty = 0;
-    var mov = 0;
-
-		horizontalAngle = 3.14;
-		verticalAngle = 0.0;
-		counter = 0;
-
-		cameraRoll.writeFloatLE(0, 0x0);
-
+    cameraRoll.writeFloatLE(0, 0x0);
+    let newAngle;
     cameraControls = setInterval(() => {
-      var now = Date.now();
-      var FrameTime = 1;
       let curFwd    = that.getFwdPosition();
       let cameraPos = that.getPosition();
-      let mousePos  = player.getMousePosition();
-      let rad       = player.getRotation();
-      let rotation  = that.getRotation();
 
-      if( checkForKeyStroke(robot.KEY_F3) ) {
-        sendMessage('CAMERA_ADD_POSITION', { pos: cameraPos, lookAt: curFwd, timeOfDay: environment.getTimeOfDay() });
+      if (checkForKeyStroke(robot.KEY_F3)) {
+        let params = { pos: cameraPos, lookAt: curFwd, timeOfDay: environment.getTimeOfDay() };
+        sendMessage('CAMERA_ADD_POSITION', params);
       }
 
-      if(checkForKeyStroke(robot.KEY_F4) ) {
+      if (checkForKeyStroke(robot.KEY_F4)) {
         sendMessage('CAMERA_CLEAR_PATH');
       }
 
-      if(checkForKeyStroke(robot.KEY_F5) ) {
+      if (checkForKeyStroke(robot.KEY_F5)) {
         sendMessage('CAMERA_PLAY');
       }
 
-      if(checkForKeyStroke(robot.KEY_F6) ) {
+      if (checkForKeyStroke(robot.KEY_F6)) {
         sendMessage('CAMERA_STOP');
       }
 
@@ -266,9 +254,9 @@ module.exports = (process, module, memory, window, player, sendMessage) => {
         if (speed < 0) speed = 1;
       }
 
-      //get radius based on the previous point and r squared = a squared + b squared
+      // get radius based on the previous point and r squared = a squared + b squared
       let r = Math.sqrt(Math.pow(curFwd.x - cameraPos.x, 2) + Math.pow(curFwd.y - cameraPos.y, 2));
-      let angle = Math.atan2((curFwd.y - cameraPos.y) , (curFwd.x - cameraPos.x));
+      let angle = Math.atan2((curFwd.y - cameraPos.y), (curFwd.x - cameraPos.x));
 
       if (Keyboard.getState(robot.KEY_Q)) {
         cameraRoll.writeFloatLE(cameraRoll.readFloatLE(0) + 0.006, 0x0);
@@ -303,8 +291,8 @@ module.exports = (process, module, memory, window, player, sendMessage) => {
         curFwd.z -= speed;
         cameraPos.z -= speed;
       }
-      //
-      if (Keyboard.getState(robot.KEY_UP) ) {
+
+      if (Keyboard.getState(robot.KEY_UP)) {
         curFwd.z = curFwd.z - 13000 * 0.0004;
       }
 
@@ -317,40 +305,40 @@ module.exports = (process, module, memory, window, player, sendMessage) => {
       dir.y = cameraPos.y - curFwd.y;
       dir.z = cameraPos.z - curFwd.z;
 
-      hyp = Math.sqrt(dir.x*dir.x + dir.y*dir.y + dir.z*dir.z);
+      let hyp = Math.sqrt(dir.x * dir.x + dir.y * dir.y + dir.z * dir.z);
       dir.x /= hyp;
       dir.y /= hyp;
       dir.z /= hyp;
 
       if (Keyboard.getState(robot.KEY_W)) {
-        curFwd.x -= dir.x*speed;
-        curFwd.y -= dir.y*speed;
-        curFwd.z -= dir.z*speed;
-        cameraPos.x -= dir.x*speed;
-        cameraPos.y -= dir.y*speed;
-        cameraPos.z -= dir.z*speed;
+        curFwd.x -= dir.x * speed;
+        curFwd.y -= dir.y * speed;
+        curFwd.z -= dir.z * speed;
+        cameraPos.x -= dir.x * speed;
+        cameraPos.y -= dir.y * speed;
+        cameraPos.z -= dir.z * speed;
       }
 
       if (Keyboard.getState(robot.KEY_S)) {
-        curFwd.x += dir.x*speed;
-        curFwd.y += dir.y*speed;
-        curFwd.z += dir.z*speed;
-        cameraPos.x += dir.x*speed;
-        cameraPos.y += dir.y*speed;
-        cameraPos.z += dir.z*speed;
+        curFwd.x += dir.x * speed;
+        curFwd.y += dir.y * speed;
+        curFwd.z += dir.z * speed;
+        cameraPos.x += dir.x * speed;
+        cameraPos.y += dir.y * speed;
+        cameraPos.z += dir.z * speed;
       }
 
 
-      const container    = getContainerDimensions();
-      const mouse        = Mouse.getPos();
-
-      halfWidth  = container.size[ 0 ] / 2;
-      halfHeight = container.size[ 1 ] / 2;
-      mousex = mouse.x;
-      mousey = mouse.y;
-
-      lastx = mousex;
-      lasty = mousey;
+      // const container    = getContainerDimensions();
+      // const mouse        = Mouse.getPos();
+      //
+      // halfWidth  = container.size[ 0 ] / 2;
+      // halfHeight = container.size[ 1 ] / 2;
+      // mousex = mouse.x;
+      // mousey = mouse.y;
+      //
+      // lastx = mousex;
+      // lasty = mousey;
 
       cameraWritePosition.writeFloatLE(cameraPos.x, 0x0);
       cameraWritePosition.writeFloatLE(cameraPos.y, 0x4);
@@ -362,12 +350,12 @@ module.exports = (process, module, memory, window, player, sendMessage) => {
       memory.writeData(cameraOffsetBase + offsets.camera.playerPos.x, cameraFwdWritePosition, 0xC);
       memory.writeData(cameraOffsetBase + offsets.camera.ortientation.roll, cameraRoll, 0x4);
     }, 17);
-  }
+  };
 
   that.getVerticalAlignment = () => {
-		memory.readData(cameraOffsetBase + offsets.camera.upOrDown, verticalAlignment, 0x4);
-		return verticalAlignment.readFloatLE(0);
-	}
+    memory.readData(cameraOffsetBase + offsets.camera.upOrDown, verticalAlignment, 0x4);
+    return verticalAlignment.readFloatLE(0);
+  };
 
   that.getRotation = () => {
     memory.readData(cameraOffsetBase + offsets.camera.ortientation.yaw, cameraYaw, 0x4);
@@ -375,7 +363,7 @@ module.exports = (process, module, memory, window, player, sendMessage) => {
     var yaw = cameraYaw.readFloatLE(0);
     var pitch = cameraPitch.readFloatLE(0);
     return { yaw: yaw, pitch: pitch };
-  }
+  };
 
   that.printDebugInfo = () => {
     setInterval(() => {
@@ -383,7 +371,7 @@ module.exports = (process, module, memory, window, player, sendMessage) => {
       var posfwd = that.getFwdPosition();
       console.log('camera.moveTo( ' + pos.x + ', ' + pos.y + ', ' + pos.z + ', ' + posfwd.x + ', ' + posfwd.y + ', ' + posfwd.z + ');');
     }, 4000);
-  }
+  };
 
   return that;
 };
