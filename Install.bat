1d8b96ac3ca565d0614b578a30caaafb9bf094c4
karliky <karliky@gmail.com>
2017-03-03 21:32:49 +0100

    Add bat scripts for easy initialization

---
+++ Install.bat
@@ -0,0 +1,5 @@
+call echo "Installing Machinima Studio dependencies"
+call cd .\gw2
+call npm install
+call cd .\gw2ui
+call npm install
---
+++ Machinima Studio.bat
@@ -0,0 +1,2 @@
+start cmd /k Call node .\gw2\index.js
+start cmd /k Call gw2ui\node_modules\electron\dist\electron.exe .\gw2ui
