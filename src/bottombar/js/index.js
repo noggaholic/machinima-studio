92a0f817eb8223039ed70998e92b7b3dd89942d8
Germán <germanrodrickson@gmail.com>
2019-05-04 13:45:06 +0200

    refactor bottom bar

--- src/bottombar/js/index.js
+++ src/bottombar/js/index.js
@@ -1,272 +1,281 @@
-var tween			= undefined;
-var isPlaying = false;
-$(document).ready(function() {
+let tween;
+let isPlaying = false;
+const {
+  $,
+  jQuery,
+  Power0,
+  TweenLite,
+  socket
+} = window;
 
-	var shell = require('electron').shell;
-	jQuery('.easingInfo').click(function(event) {
-		event.preventDefault();
-		shell.openExternal(this.href);
-	});
-
-	var setPlay = function() {
-		$('.play').html('<span class="glyphicon glyphicon-play"></span> Play');
-		isPlaying = false;
-	};
-
-	var setPlaying = function() {
-		$('.play').html('<span class="glyphicon glyphicon-pause"></span> Playing');
-		isPlaying = true;
-	};
-
-	var stopTween = function() {
-		if (tween) {
-			tween.pause();
-			setPlay();
-		}
-	};
-
-	$(".stop").click(function() {
-		stopTween();
-	});
-
-	var easeBase 	= $('#easeBase');
-	var easeSub 	= $('#easeSub');
-	var subConfig = $('#subConfig');
-
-	var getEasing = function() {
-		var base = window;
-		base = base[$('#easeBase option:selected').val()][$('#easeSub option:selected').val()];
-
-		var currBase = $('#easeBase option:selected').val();
-		var currAnim = easing.filter(function(item){ return item.base === currBase; })[0];
-
-		if (Array.isArray(currAnim.configurable)) {
-			if(currAnim.base === 'RoughEase') {
-				base = base.config(currAnim.configurable);
-			} else {
-				var args = [];
-				var total = currAnim.configurable.length;
-				subConfig.find('input').each(function(key, item){
-					args.push(parseFloat(item.value));
-				});
-				base = base.config.apply(this, args);
-			}
-		}
-
-		return base;
-	}
-
-	var cinematicValues = {
-		x: 0,
-		y: 0,
-		z: 0,
-		lookAtx: 0,
-		lookAty: 0,
-		lookAty: 0,
-		timeOfDay: 0,
-	};
-
-	var sendWaypointData = function() {
-		var path = [];
-		var rows = $(".waypoints > tbody > tr");
-		var duration = parseInt($(".durationSelector input").val());
-		if (rows.length <= 1){
-			return;
-		}
-
-		rows.each(function(index, row) {
-			row = $(row);
-			var x = parseFloat($(row.children()[1]).find("textarea").val());
-			var y = parseFloat($(row.children()[2]).find("textarea").val());
-			var z = parseFloat($(row.children()[3]).find("textarea").val());
-			var lookAtx = parseFloat($(row.children()[4]).find("textarea").val());
-			var lookAty = parseFloat($(row.children()[5]).find("textarea").val());
-			var lookAtz = parseFloat($(row.children()[6]).find("textarea").val());
-
-			var roll 		= parseFloat($(row.children()[7]).find("textarea").val());
-			var timeOfDay = parseFloat($(row.children()[8]).find("textarea").val());
-			var frameRate = parseFloat($(row.children()[9]).find("textarea").val());
-			path.push({ x: x, y: y, z: z, lookAtx: lookAtx, lookAty:lookAty, lookAtz: lookAtz, timeOfDay: timeOfDay, roll: roll, frameRate: frameRate});
-		}).promise().done( function() {
-
-			cinematicValues.x = path[0].x;
-			cinematicValues.y = path[0].y;
-			cinematicValues.z = path[0].z;
-			cinematicValues.lookAtx = path[0].lookAtx;
-			cinematicValues.lookAty = path[0].lookAty;
-			cinematicValues.lookAtz = path[0].lookAtz;
-
-			cinematicValues.timeOfDay = path[0].timeOfDay;
-			cinematicValues.roll = path[0].roll;
-			cinematicValues.frameRate = path[0].frameRate;
-
-			tween =	TweenLite.to(cinematicValues, duration, {
-				bezier: {
-					values: path,
-					curviness: 0,
-					type: "soft",
-					timeResolution: 0
-				},
-				ease: getEasing(),
-				onComplete: function() {
-					setPlay();
-				},
-				onUpdate: function() {
-					socket.emit('CAMERA_SET_POS', {
-						x: cinematicValues.x,
-						y: cinematicValues.y,
-						z: cinematicValues.z ,
-						lookAtx: cinematicValues.lookAtx,
-						lookAty: cinematicValues.lookAty,
-						lookAtz: cinematicValues.lookAtz,
-						timeOfDay: cinematicValues.timeOfDay,
-						roll: cinematicValues.roll,
-						frameRate: cinematicValues.frameRate
-					});
-				}
-			});
-		});
-
-	};
-
-	$(document).on("click", ".deleteWaypoint",function() {
-		this.closest('tr').remove();
-	});
-
-	var easing = [
-		{
-			base: 'Power0',
-			sub:  'easeNone',
-			configurable: null
-		},
-		{
-			base: 'Power1',
-			sub:  ['easeIn', 'easeInOut', 'easeOut'],
-			configurable: null
-		},
-		{
-			base: 'Power2',
-			sub:  ['easeIn', 'easeInOut', 'easeOut'],
-			configurable: null
-		},
-		{
-			base: 'Power3',
-			sub:  ['easeIn', 'easeInOut', 'easeOut'],
-			configurable: null
-		},
-		{
-			base: 'Power4',
-			sub:  ['easeIn', 'easeInOut', 'easeOut'],
-			configurable: null
-		},
-		{
-			base: 'Back',
-			sub:  ['easeIn', 'easeInOut', 'easeOut'],
-			configurable: [{ placeholder: 'overshoot ' }]
-		},
-		{
-			base: 'Elastic',
-			sub:  ['easeIn', 'easeInOut', 'easeOut'],
-			configurable: [{ placeholder: 'amplitude (1.1)' }, { placeholder: 'period (0.4)' }]
-		},
-		{
-			base: 'Bounce',
-			sub:  ['easeIn', 'easeInOut', 'easeOut'],
-			configurable: null
-		},
-		{
-			base: 'RoughEase',
-			sub:  'ease',
-			configurable: { template:  Power0.easeNone, strength: 1, points: 20, taper: "none", randomize: true, clamp: false }
-		},
-		{
-			base: 'SlowMo',
-			sub:  'ease',
-			configurable: [{ placeholder: 'linearRatio (0.5)' }, { placeholder: 'power (0.8)' }, { placeholder: 'yoyoMode (1 or 0)' }]
-		},
-		{
-			base: 'SteppedEase',
-			sub:  null,
-			configurable: [{ placeholder: 'n of steps' }]
-		},
-		{
-			base: 'Circ',
-			sub:  ['easeIn', 'easeInOut', 'easeOut'],
-			configurable: null
-		},
-		{
-			base: 'Expo',
-			sub:  ['easeIn', 'easeInOut', 'easeOut'],
-			configurable: null
-		},
-		{
-			base: 'Sine',
-			sub:  ['easeIn', 'easeInOut', 'easeOut'],
-			configurable: null
-		}
+const easing = [
+  {
+    base: 'Power0',
+    sub: 'easeNone',
+    configurable: null
+  },
+  {
+    base: 'Power1',
+    sub: ['easeIn', 'easeInOut', 'easeOut'],
+    configurable: null
+  },
+  {
+    base: 'Power2',
+    sub: ['easeIn', 'easeInOut', 'easeOut'],
+    configurable: null
+  },
+  {
+    base: 'Power3',
+    sub: ['easeIn', 'easeInOut', 'easeOut'],
+    configurable: null
+  },
+  {
+    base: 'Power4',
+    sub: ['easeIn', 'easeInOut', 'easeOut'],
+    configurable: null
+  },
+  {
+    base: 'Back',
+    sub: ['easeIn', 'easeInOut', 'easeOut'],
+    configurable: [{ placeholder: 'overshoot ' }]
+  },
+  {
+    base: 'Elastic',
+    sub: ['easeIn', 'easeInOut', 'easeOut'],
+    configurable: [{ placeholder: 'amplitude (1.1)' }, { placeholder: 'period (0.4)' }]
+  },
+  {
+    base: 'Bounce',
+    sub: ['easeIn', 'easeInOut', 'easeOut'],
+    configurable: null
+  },
+  {
+    base: 'RoughEase',
+    sub: 'ease',
+    configurable: {
+      template: Power0.easeNone,
+      strength: 1,
+      points: 20,
+      taper: 'none',
+      randomize: true,
+      clamp: false
+    }
+  },
+  {
+    base: 'SlowMo',
+    sub: 'ease',
+    configurable: [{ placeholder: 'linearRatio (0.5)' }, { placeholder: 'power (0.8)' }, { placeholder: 'yoyoMode (1 or 0)' }]
+  },
+  {
+    base: 'SteppedEase',
+    sub: null,
+    configurable: [{ placeholder: 'n of steps' }]
+  },
+  {
+    base: 'Circ',
+    sub: ['easeIn', 'easeInOut', 'easeOut'],
+    configurable: null
+  },
+  {
+    base: 'Expo',
+    sub: ['easeIn', 'easeInOut', 'easeOut'],
+    configurable: null
+  },
+  {
+    base: 'Sine',
+    sub: ['easeIn', 'easeInOut', 'easeOut'],
+    configurable: null
+  }
 ];
 
-	var applyEasing = function() {
-		$.each(easing, function( key, item ) {
-			easeBase.append($('<option>', {
-	      value	: item.base,
-	      text 	: item.base
-	    }));
-		});
-		easeBase.multiselect({
-			onChange: function(option, checked) {
-	      easeSub.empty();
-				applySubAnimation();
+$(document).ready(() => {
+  const { shell } = require('electron');
+  jQuery('.easingInfo').click((event) => {
+    event.preventDefault();
+    shell.openExternal(this.href);
+  });
+
+  const setPlay = () => {
+    $('.play').html('<span class="glyphicon glyphicon-play"></span> Play');
+    isPlaying = false;
+  };
+
+  const setPlaying = () => {
+    $('.play').html('<span class="glyphicon glyphicon-pause"></span> Playing');
+    isPlaying = true;
+  };
+
+  const stopTween = () => {
+    if (tween) {
+      tween.pause();
+      setPlay();
+    }
+  };
+
+  $('.stop').click(() => stopTween());
+
+  const easeBase = $('#easeBase');
+  const easeSub = $('#easeSub');
+  const subConfig = $('#subConfig');
+
+  const getEasing = () => {
+    let base = window;
+    base = base[$('#easeBase option:selected').val()][$('#easeSub option:selected').val()];
+
+    const currBase = $('#easeBase option:selected').val();
+    const [currAnim] = easing.filter(item => item.base === currBase);
+
+    if (Array.isArray(currAnim.configurable)) {
+      if (currAnim.base === 'RoughEase') {
+        base = base.config(currAnim.configurable);
+      } else {
+        const args = [];
+        subConfig.find('input').each((key, item) => {
+          args.push(parseFloat(item.value));
+        });
+        base = base.config.apply(this, args);
       }
-		});
-	};
+    }
 
-	var applySubAnimation = function() {
-		var currBase = $('#easeBase option:selected').val();
-		var currAnim = easing.filter(function(item){ return item.base === currBase; })[0];
+    return base;
+  };
 
-		if (typeof currAnim.sub === 'object') {
-			$.each(currAnim.sub, function( key, item ) {
-				easeSub.append($('<option>', {
-		      value	: item,
-		      text 	: item
-		    }));
-			});
-		}
+  const cinematicValues = {
+    x: 0,
+    y: 0,
+    z: 0,
+    lookAtx: 0,
+    lookAty: 0,
+    lookAtz: 0,
+    timeOfDay: 0
+  };
 
-		if (typeof currAnim.sub === 'string') {
-			easeSub.append($('<option>', {
-				value	: currAnim.sub,
-				text 	: currAnim.sub
-			}));
-		}
+  const sendWaypointData = () => {
+    const path = [];
+    const rows = $('.waypoints > tbody > tr');
+    const duration = parseInt($('.durationSelector input').val(), 10);
+    if (rows.length <= 1) return;
 
-		subConfig.empty();
-		if ( Object.prototype.toString.call(currAnim.configurable) === '[object Array]') {
-			var total = currAnim.configurable.length;
-			var item 	= null;
-			for (var i = 0; i < total; i++) {
-				item = currAnim.configurable[i];
-				var input = document.createElement("input");
-				input.type = "text"
-				input.placeholder = item.placeholder
-				subConfig.append(input);
-			}
-		}
+    rows.each((index, row) => {
+      row = $(row);
+      const x = parseFloat($(row.children()[1]).find('textarea').val());
+      const y = parseFloat($(row.children()[2]).find('textarea').val());
+      const z = parseFloat($(row.children()[3]).find('textarea').val());
+      const lookAtx = parseFloat($(row.children()[4]).find('textarea').val());
+      const lookAty = parseFloat($(row.children()[5]).find('textarea').val());
+      const lookAtz = parseFloat($(row.children()[6]).find('textarea').val());
 
-		easeSub.multiselect('rebuild');
-	};
+      const roll = parseFloat($(row.children()[7]).find('textarea').val());
+      const timeOfDay = parseFloat($(row.children()[8]).find('textarea').val());
+      const frameRate = parseFloat($(row.children()[9]).find('textarea').val());
+      path.push({
+        x,
+        y,
+        z,
+        lookAtx,
+        lookAty,
+        lookAtz,
+        timeOfDay,
+        roll,
+        frameRate
+      });
+    }).promise().done(() => {
+      cinematicValues.x = path[0].x;
+      cinematicValues.y = path[0].y;
+      cinematicValues.z = path[0].z;
+      cinematicValues.lookAtx = path[0].lookAtx;
+      cinematicValues.lookAty = path[0].lookAty;
+      cinematicValues.lookAtz = path[0].lookAtz;
 
-	applyEasing();
-	applySubAnimation();
-	easeSub.multiselect();
-	$('.play').click(function() {
-		if (isPlaying) {
-			stopTween();
-			setPlay();
-		} else {
-			setPlaying();
-			sendWaypointData();
-		}
-	});
+      cinematicValues.timeOfDay = path[0].timeOfDay;
+      cinematicValues.roll = path[0].roll;
+      cinematicValues.frameRate = path[0].frameRate;
+
+      tween = TweenLite.to(cinematicValues, duration, {
+        bezier: {
+          values: path,
+          curviness: 0,
+          type: 'soft',
+          timeResolution: 0
+        },
+        ease: getEasing(),
+        onComplete: setPlay,
+        onUpdate: () => {
+          socket.emit('CAMERA_SET_POS', {
+            x: cinematicValues.x,
+            y: cinematicValues.y,
+            z: cinematicValues.z,
+            lookAtx: cinematicValues.lookAtx,
+            lookAty: cinematicValues.lookAty,
+            lookAtz: cinematicValues.lookAtz,
+            timeOfDay: cinematicValues.timeOfDay,
+            roll: cinematicValues.roll,
+            frameRate: cinematicValues.frameRate
+          });
+        }
+      });
+    });
+  };
+
+  $(document).on('click', '.deleteWaypoint', () => {
+    this.closest('tr').remove();
+  });
+
+  const applySubAnimation = () => {
+    const currBase = $('#easeBase option:selected').val();
+    const [currAnim] = easing.filter(item => item.base === currBase);
+
+    if (typeof currAnim.sub === 'object') {
+      $.each(currAnim.sub, (key, item) => {
+        easeSub.append($('<option>', { value: item, text: item }));
+      });
+    }
+
+    if (typeof currAnim.sub === 'string') {
+      easeSub.append($('<option>', { value: currAnim.sub, text: currAnim.sub }));
+    }
+
+    subConfig.empty();
+    if (Object.prototype.toString.call(currAnim.configurable) === '[object Array]') {
+      const total = currAnim.configurable.length;
+      let item;
+      for (let i = 0; i < total; i++) {
+        item = currAnim.configurable[i];
+        const input = document.createElement('input');
+        input.type = 'text';
+        input.placeholder = item.placeholder;
+        subConfig.append(input);
+      }
+    }
+
+    easeSub.multiselect('rebuild');
+  };
+
+  const applyEasing = () => {
+    $.each(easing, (key, item) => {
+      easeBase.append($('<option>', {
+        value: item.base,
+        text: item.base
+      }));
+    });
+    easeBase.multiselect({
+      onChange: () => {
+        easeSub.empty();
+        applySubAnimation();
+      }
+    });
+  };
+
+  applyEasing();
+  applySubAnimation();
+  easeSub.multiselect();
+  $('.play').click(() => {
+    if (isPlaying) {
+      stopTween();
+      setPlay();
+    } else {
+      setPlaying();
+      sendWaypointData();
+    }
+  });
 });
--- src/sidebar/js/connect.js
+++ src/sidebar/js/connect.js
@@ -1,233 +1,232 @@
+/* eslint-disable no-shadow */
+const { io, $ } = window;
+
 window.socket = io('http://127.0.0.1:8080');
-socket.on('error', function (err) {
-   console.log("Socket.IO Error");
-   console.log(err); // this is changed from your code in last comment
-});
-$(".rendering").change(function() {
+const { socket } = window;
 
-	switch ($(this).data( "id" )) {
-		case 'audio':
-			socket.emit('RENDERING', { section: 'AUDIO' });
-			break;
-		case 'blocks':
-			socket.emit('RENDERING', { section: 'BLOCKS' });
-			break;
-		case 'decal':
-			socket.emit('RENDERING', { section: 'DECAL' });
-			break;
-		case 'environment':
-			socket.emit('RENDERING', { section: 'ENVIRONMENT' });
-			break;
-		case 'river':
-			socket.emit('RENDERING', { section: 'RIVER' });
-			break;
-		case 'under_water':
-			socket.emit('RENDERING', { section: 'UNDER_WATER' });
-			break;
-		case 'zone':
-			socket.emit('RENDERING', { section: 'ZONE' });
-			break;
-		case 'lights':
-			socket.emit('RENDERING', { section: 'LIGHTS' });
-			break;
-		case 'shadows':
-			socket.emit('RENDERING', { section: 'SHADOWS' });
-			break;
-		case 'terrain':
-			socket.emit('RENDERING', { section: 'TERRAIN' });
-			break;
-		case 'cube_map':
-			socket.emit('RENDERING', { section: 'CUBE_MAP' });
-			break;
-		case 'props':
-			socket.emit('RENDERING', { section: 'PROPS' });
-			break;
-		case 'animation':
-			socket.emit('RENDERING', { section: 'ANIMATION' });
-			break;
-        case 'highlight_effect':
-			socket.emit('RENDERING', { section: 'HIGHLIGHT_EFFECT' });
-			break;
-		}
+socket.on('error', (err) => {
+  console.log('Socket.IO Error');
+  console.log(err); // this is changed from your code in last comment
 });
 
-$("#enableFlying").change(function() {
-	if(this.checked) {
-		socket.emit('PLAYER_ENABLE_FLY');
-	} else {
-		socket.emit('PLAYER_DISABLE_FLY');
-	}
+$('.rendering').change(() => {
+  switch ($(this).data('id')) {
+    case 'audio':
+      socket.emit('RENDERING', { section: 'AUDIO' });
+      break;
+    case 'blocks':
+      socket.emit('RENDERING', { section: 'BLOCKS' });
+      break;
+    case 'decal':
+      socket.emit('RENDERING', { section: 'DECAL' });
+      break;
+    case 'environment':
+      socket.emit('RENDERING', { section: 'ENVIRONMENT' });
+      break;
+    case 'river':
+      socket.emit('RENDERING', { section: 'RIVER' });
+      break;
+    case 'under_water':
+      socket.emit('RENDERING', { section: 'UNDER_WATER' });
+      break;
+    case 'zone':
+      socket.emit('RENDERING', { section: 'ZONE' });
+      break;
+    case 'lights':
+      socket.emit('RENDERING', { section: 'LIGHTS' });
+      break;
+    case 'shadows':
+      socket.emit('RENDERING', { section: 'SHADOWS' });
+      break;
+    case 'terrain':
+      socket.emit('RENDERING', { section: 'TERRAIN' });
+      break;
+    case 'cube_map':
+      socket.emit('RENDERING', { section: 'CUBE_MAP' });
+      break;
+    case 'props':
+      socket.emit('RENDERING', { section: 'PROPS' });
+      break;
+    case 'animation':
+      socket.emit('RENDERING', { section: 'ANIMATION' });
+      break;
+    case 'highlight_effect':
+      socket.emit('RENDERING', { section: 'HIGHLIGHT_EFFECT' });
+      break;
+    default:
+      break;
+  }
+});
+
+$('#enableFlying').change(() => {
+  if (this.checked) {
+    socket.emit('PLAYER_ENABLE_FLY');
+  } else {
+    socket.emit('PLAYER_DISABLE_FLY');
+  }
 });
 
 
 // Initialize the marker position and the active class
-var marker = $('#marker'),
-    current = $('.active');
+const marker = $('#marker');
+const current = $('.active');
 marker.css({
-    // Place the marker in the middle of the border
-    bottom: -(marker.height() / 2),
-    left: current.position().left,
-    width: current.outerWidth(),
-    display: "block"
+  // Place the marker in the middle of the border
+  bottom: -(marker.height() / 2),
+  left: current.position().left,
+  width: current.outerWidth(),
+  display: 'block'
 });
 
 
-$(".menu button").click(function() {
-  //$(".menu button").removeClass('active');
-  //$(this).addClass('active');
-
-
-
-  var self = $(this),
-  offsetLeft = self.position().left,
-  width = self.outerWidth() || current.outerWidth(),
-  left = offsetLeft == 0 ? 0 : offsetLeft || current.position().left;
-  $(".menu button").removeClass('active');
+$('.menu button').click(() => {
+  // $(".menu button").removeClass('active');
+  // $(this).addClass('active');
+  const self = $(this);
+  const offsetLeft = self.position().left;
+  const width = self.outerWidth() || current.outerWidth();
+  const left = offsetLeft === 0 ? 0 : offsetLeft || current.position().left;
+  $('.menu button').removeClass('active');
   self.addClass('active');
-    marker.css({
-        left: left,
-        width: width,
-    });
-
+  marker.css({ left, width });
 });
 
-$("#enableTimeOfDay").change(function() {
-	if(this.checked) {
-		socket.emit('ENV_ENABLE_TIME_OF_DAY');
-		$(".timeOfDay").removeAttr('disabled');
-	} else {
-		socket.emit('ENV_DISABLE_TIME_OF_DAY');
-		$(".timeOfDay").attr('disabled', 'disabled');
-	}
+$('#enableTimeOfDay').change(() => {
+  if (this.checked) {
+    socket.emit('ENV_ENABLE_TIME_OF_DAY');
+    $('.timeOfDay').removeAttr('disabled');
+  } else {
+    socket.emit('ENV_DISABLE_TIME_OF_DAY');
+    $('.timeOfDay').attr('disabled', 'disabled');
+  }
 });
 
-$(".timeOfDay").on("input change", function() {
-	var density = $(this).val();
-	socket.emit('ENV_SET_TIME_OF_DAY', density);
+$('.timeOfDay').on('input change', () => {
+  const density = $(this).val();
+  socket.emit('ENV_SET_TIME_OF_DAY', density);
 });
 const fpsCounter = $('.fpsCounter');
-$(".bulletTime").on("input change", function() {
-	const frameRate = $(this).val();
-  fpsCounter.html('Bullet time / Frame rate (' + frameRate + 'fps)');
-	socket.emit('ANIM_SET_FRAME_RATE', frameRate);
+$('.bulletTime').on('input change', () => {
+  const frameRate = $(this).val();
+  fpsCounter.html(`Bullet time / Frame rate (${frameRate}fps)`);
+  socket.emit('ANIM_SET_FRAME_RATE', frameRate);
 });
 
-$(".enableCamera").change(function() {
-    if(this.checked) {
-      socket.emit('CAMERA_ENABLE_CONTROLS');
-    } else {
-      socket.emit('CAMERA_DISABLE_CONTROLS');
-    }
+$('.enableCamera').change(() => {
+  if (this.checked) {
+    socket.emit('CAMERA_ENABLE_CONTROLS');
+  } else {
+    socket.emit('CAMERA_DISABLE_CONTROLS');
+  }
 });
 
-let cameraTable    = $('.cameraTable tbody');
+const cameraTable = $('.cameraTable tbody');
 let cameraCounter = 0;
 
-$(".advance").click(() => {
+$('.advance').click(() => {
   socket.emit('PLAYER_ADVANCE_IN_DIRECTION');
 });
-$(".stepdown").click(() => {
+$('.stepdown').click(() => {
   socket.emit('PLAYER_DOWN');
 });
-$(".stepup").click(() => {
+$('.stepup').click(() => {
   socket.emit('PLAYER_UP');
 });
-$(".play").click(() => {
-  var positions = [];
-  var listOfPositions = cameraTable.find('tr');
-  listOfPositions.each((i,tr) => {
-    var data = [];
+$('.play').click(() => {
+  const positions = [];
+  const listOfPositions = cameraTable.find('tr');
+  listOfPositions.each((i, tr) => {
+    const data = [];
     $(tr).find('td').each((i, td) => {
       data.push(parseFloat($(td).text()));
-    })
+    });
     positions.push(data);
   });
-  socket.emit('CAMERA_TWEEN_TO',positions);
+  socket.emit('CAMERA_TWEEN_TO', positions);
 });
 
-	socket.on('CAMERA_PLAY', () => {
-		$(".play").click();
-	});
+socket.on('CAMERA_PLAY', () => {
+  $('.play').click();
+});
 
-	var cameraInfo = $('.cameraTable>.list-group-item>span');
-	socket.on('UPDATE_UI', (data) => {
-		cameraInfo[0].innerHTML = data.x;
-		cameraInfo[1].innerHTML = data.y;
-		cameraInfo[2].innerHTML = data.z;
-		cameraInfo[3].innerHTML = data.lookAtx;
-		cameraInfo[4].innerHTML = data.lookAty;
-		cameraInfo[5].innerHTML = data.lookAtz;
-		cameraInfo[6].innerHTML = data.roll;
-		cameraInfo[7].innerHTML = data.speed;
-		cameraInfo[8].innerHTML = data.rotSpeed;
-		cameraInfo[9].innerHTML = data.up_down_speed;
+const cameraInfo = $('.cameraTable>.list-group-item>span');
+socket.on('UPDATE_UI', (data) => {
+  cameraInfo[0].innerHTML = data.x;
+  cameraInfo[1].innerHTML = data.y;
+  cameraInfo[2].innerHTML = data.z;
+  cameraInfo[3].innerHTML = data.lookAtx;
+  cameraInfo[4].innerHTML = data.lookAty;
+  cameraInfo[5].innerHTML = data.lookAtz;
+  cameraInfo[6].innerHTML = data.roll;
+  cameraInfo[7].innerHTML = data.speed;
+  cameraInfo[8].innerHTML = data.rotSpeed;
+  cameraInfo[9].innerHTML = data.up_down_speed;
 
-        $("#flat").spectrum("set", 'rgb(' + data.fog_red + ',' + data.fog_green + "," + data.fog_blue + ')');
-        $(".fogDensity").val(data.fog_density);
-        $(".timeOfDay").val(data.time_of_day);
-	});
+  $('#flat').spectrum('set', `rgb(${data.fog_red}, ${data.fog_green}, ${data.fog_blue})`);
+  $('.fogDensity').val(data.fog_density);
+  $('.timeOfDay').val(data.time_of_day);
+});
 
-	var debugInfo = $('.debugInfo>.list-group-item>span');
-	socket.on('UPDATE_DEBUG_INFO', (data) => {
-		debugInfo[0].innerHTML = data.MapName;
-		debugInfo[1].innerHTML = data.MapNamespace;
-		debugInfo[2].innerHTML = data.MapSector;
-		debugInfo[3].innerHTML = data.MapType;
-		debugInfo[4].innerHTML = data.MapFloor;
-		debugInfo[5].innerHTML = data.MapId;
-		debugInfo[6].innerHTML = data.MapGuid;
-	});
+const debugInfo = $('.debugInfo>.list-group-item>span');
+socket.on('UPDATE_DEBUG_INFO', (data) => {
+  debugInfo[0].innerHTML = data.MapName;
+  debugInfo[1].innerHTML = data.MapNamespace;
+  debugInfo[2].innerHTML = data.MapSector;
+  debugInfo[3].innerHTML = data.MapType;
+  debugInfo[4].innerHTML = data.MapFloor;
+  debugInfo[5].innerHTML = data.MapId;
+  debugInfo[6].innerHTML = data.MapGuid;
+});
 
-	socket.on('CAMERA_ADD_POSITION', (data) => {
-	  let templ = `<tr><th scope='row'>${cameraCounter}</th>
-	  <td>${data.pos.x}</td>
-	  <td>${data.pos.y}</td>
-	  <td>${data.pos.z}</td>
-	  <td>${data.lookAt.x}</td>
-	  <td>${data.lookAt.y}</td>
-	  <td>${data.lookAt.z}</td></tr>`;
-	  cameraCounter++;
-	  cameraTable.append(templ);
-	});
+socket.on('CAMERA_ADD_POSITION', (data) => {
+  const templ = `<tr><th scope='row'>${cameraCounter}</th>
+<td>${data.pos.x}</td>
+<td>${data.pos.y}</td>
+<td>${data.pos.z}</td>
+<td>${data.lookAt.x}</td>
+<td>${data.lookAt.y}</td>
+<td>${data.lookAt.z}</td></tr>`;
+  cameraCounter++;
+  cameraTable.append(templ);
+});
 
-$(document).ready(function() {
-
-  $(".btn-pref .btn").click(function () {
-      $(".btn-pref .btn").removeClass("btn-primary").addClass("btn-default");
-      // $(".tab").addClass("active"); // instead of this do the below
-      $(this).removeClass("btn-default").addClass("btn-primary");
+$(document).ready(() => {
+  $('.btn-pref .btn').click(() => {
+    $('.btn-pref .btn').removeClass('btn-primary').addClass('btn-default');
+    // $(".tab").addClass("active"); // instead of this do the below
+    $(this).removeClass('btn-default').addClass('btn-primary');
   });
 
-	$(".fogDensity").on("input change", function() {
-    var density = $(this).val();
+  $('.fogDensity').on('input change', () => {
+    const density = $(this).val();
     socket.emit('ENV_SET_FOG_DENSITY', density);
   });
 
-	$(".speedRange").on("input change", function() {
-		var speed = $(this).val();
-		cameraInfo[7].innerHTML = speed;
+  $('.speedRange').on('input change', () => {
+    const speed = $(this).val();
+    cameraInfo[7].innerHTML = speed;
     socket.emit('CAMERA_SET_SPEED', speed);
-	});
+  });
 
-	$(".rotSpeedRange").on("input change", function() {
-		var speed = $(this).val();
-		cameraInfo[8].innerHTML = speed;
+  $('.rotSpeedRange').on('input change', () => {
+    const speed = $(this).val();
+    cameraInfo[8].innerHTML = speed;
     socket.emit('CAMERA_SET_ROT_SPEED', speed);
-	});
+  });
 
-	$(".up_down_speed").on("input change", function() {
-		var speed = $(this).val();
-		cameraInfo[9].innerHTML = speed;
+  $('.up_down_speed').on('input change', () => {
+    const speed = $(this).val();
+    cameraInfo[9].innerHTML = speed;
     socket.emit('CAMERA_SET_UP_DOWN_SPEED', speed);
-	});
+  });
 
-	$("#flat").spectrum({
-	    flat: true,
-			preferredFormat: "hex",
-	    showInput: true,
-			move: function(color) {
-				color = color.toRgb();
-				socket.emit('ENV_SET_FOG_COLOR', { r: color.r, g: color.g, b: color.b });
-			}
-	});
+  $('#flat').spectrum({
+    flat: true,
+    preferredFormat: 'hex',
+    showInput: true,
+    move(color) {
+      color = color.toRgb();
+      socket.emit('ENV_SET_FOG_COLOR', { r: color.r, g: color.g, b: color.b });
+    }
+  });
 });
